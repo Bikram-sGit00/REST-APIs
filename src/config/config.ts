e363148d4e6fe54989a5cea7009f1e3461038c42
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-11 14:04:28 +0530

    add environment file

---
+++ src/config/config.ts
@@ -0,0 +1,6 @@
+const _config={  // '_' is used to indicate that this is a private variable
+    port:process.env.PORT,
+}
+
+export const config = Object.freeze(_config); // freeze is an javascript object method,'object.freeze' is used to make the object read-only...
+
