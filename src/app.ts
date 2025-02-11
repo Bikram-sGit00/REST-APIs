f0cefc90c31da519a7358cf093384a9395d2c1ab
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-11 12:37:37 +0530

    setup express server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from "express";
 
-const app = express;
+const app = express();
+
+app.get("/", (req, res, next) => {
+    res.json({ message: "Hello World" });
+ });
 
 export default app;
