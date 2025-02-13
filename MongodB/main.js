719f212be15d30f1b037a7645772b25a137987df
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-13 21:34:29 +0530

    create a model file

--- MongodB/main.js
+++ MongodB/main.js
@@ -1,1 +1,16 @@
 import mongoose from "mongoose";
+import express from "express";
+import { Todo } from "./models/TODO.js";
+
+let a = await mongoose.connect("mongodb://localhost:27017/");
+
+let app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
