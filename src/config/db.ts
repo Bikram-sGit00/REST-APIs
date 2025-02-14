6b391c7742a3ef6212bcad39c467d734483843ea
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-14 11:45:34 +0530

    connect our database to server

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,14 +4,19 @@ import { config } from "./config";
 const connectDB = async () => {
     try {
         await mongoose.connect(config.databaseUrl as string) // string is used to tell the ts compiler that the value will surely come , this is called type casting...
+
+        mongoose.connection.on("connected", () => {  //Mongoose returns some events like connected, error, disconnected, etc. & We can listen to these events...
+            console.log("Connected to the database");  
+        });
+        mongoose.connection.on("error", (err) => { //
+            console.log("Error connecting to the database in future", err);
+        });
     
     } catch (error) {
         console.log("Error connecting to the database", error);
+        process.exit(1); // status code 1 is used to tell the process that it has ended with an error...
         
     }
 }
 
-
-
-
-
+export default connectDB;
