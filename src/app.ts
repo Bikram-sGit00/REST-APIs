d97638523fa080b249906968595460b083e1b48e
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-14 14:56:53 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
-import express from "express";
+import express, {Request,Response ,NextFunction } from "express";
+import { HttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
     res.json({ message: "Hello World" });
  });
+
+ //Global error handler is a special type of express construct that has four parameters, the first one is the error object, the second one is the request object, the third one is the response object, and the fourth one is the next function...
+
+ app.use((err:HttpError,req:Request,res:Response,next:NextFunction)=>{
+    const statusCode=err.statusCode || 500;
+    return res.status(statusCode).json({
+        message:err.message
+    })
+ }) // "use" is used to register a middleware function with the application...
+
 
 export default app;
