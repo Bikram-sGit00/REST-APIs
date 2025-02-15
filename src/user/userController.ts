e3095e554c6ec2b9671ed2b0b58748bb49eda840
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-15 22:35:54 +0530

    add more security

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body; //destructuring, will get the values in req and for getting it we use req.body...
@@ -15,6 +16,8 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "User already exists");
     return next(error);
   }
+  //password hashing
+  const hassedPassword =await bcrypt.hashSync(password, 10);//Here 10 is salt rounds,also in matter of salt rounds 10 is sweet spot...
   //process
   //response
   res.json({ message: "User created successfully!" });
