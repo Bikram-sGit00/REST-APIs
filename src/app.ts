c6362930b84f4049fb3abad97488d7be3df9f466
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-15 11:18:06 +0530

    minor changes in file

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,15 @@
-import express, { Request, Response, NextFunction } from "express";
-import createHttpError, { HttpError } from "http-errors";
-import { config } from "./config/config";
+import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req, res, next) => {
-  // throw new Error("we can throw error in JS like this");
-
-  const error = createHttpError(500, "Own Error");
-  throw error;
-
-  res.json({ message: "Hello World" });
+  //   // throw new Error("we can throw error in JS like this");
+  //   const error = createHttpError(500, "Own Error");
+  //   throw error;
+  res.json({ message: "Welcome!" });
 });
 
-//Global error handler is a special type of express construct that has four parameters, the first one is the error object, the second one is the request object, the third one is the response object, and the fourth one is the next function...
-
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-  const statusCode = err.statusCode || 500;
-  res.status(statusCode).json({
-    message: err.message, //used to send the error message to the client...
-    errorStack:
-      config.env === "development"
-        ? err.stack
-        : "Can't Show To Unauthorized User", //error stack gives us the whole information about the error...
-  });
-}); // "use" is used to register a middleware function with the application...
+app.use(globalErrorHandler);
 
 export default app;
