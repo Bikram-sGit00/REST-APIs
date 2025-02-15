7eecc9e439135adc058660b11c4993dc5ac57196
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-15 22:14:26 +0530

    add user model

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,11 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body; //destructuring, will get the values in req and for getting it we use req.body...
   //validation
   if (!name || !email || !password) {
     const error = createHttpError(400, "Please provide all the details");
+    return next(error);
+  }
+  //database operation
+  const user = await userModel.findOne({ email });
+  if (user) {
+    const error = createHttpError(400, "User already exists");
     return next(error);
   }
   //process
