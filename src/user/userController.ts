f702f3278d06a05fff195224e73b5b1b766e11d8
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-17 01:03:06 +0530

    server-side error handleing

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,6 +4,7 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body; //destructuring, will get the values in req and for getting it we use req.body...
@@ -12,32 +13,50 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "Please provide all the details");
     return next(error);
   }
+
   //database operation
-  const user = await userModel.findOne({ email });
-  if (user) {
-    const error = createHttpError(400, "User already exists");
-    return next(error);
+  let newUser: User | null = null;
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (user) {
+      const error = createHttpError(400, "User already exists");
+      return next(error);
+    }
+  } catch (err) {
+    return next(
+      createHttpError(500, "Server error while getting user details")
+    );
   }
+
   //password hashing
   const hassedPassword = await bcrypt.hashSync(password, 10); //Here 10 is salt rounds,also in matter of salt rounds 10 is sweet spot...
 
-  // const newUser = new userModel({ name, email, password: hassedPassword });  //OR
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hassedPassword,
-  }); //create is a mongoose method which will create a new document in the collection...
+  try {
+    // const newUser = new userModel({ name, email, password: hassedPassword });  //OR
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hassedPassword,
+    }); //create is a mongoose method which will create a new document in the collection...
+  } catch (err) {
+    return next(createHttpError(500, "Server error @password hashing"));
+  }
 
-  //JWT token generation
-  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
-    expiresIn: "7d", //    ^
-    algorithm: "HS256", // |----> these are the options which we can pass to sign method,also we can leave it empty it will take default values...
-  }); //                   |----> sub is a subject,which is a standard claim of JWT token,which is used to identify the subject of the JWT token...
-  //sign is a method from jsonwebtoken module,which will generate a token...
+  try {
+    //JWT token generation
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d", //    ^
+      algorithm: "HS256", // |----> these are the options which we can pass to sign method,also we can leave it empty it will take default values...
+    }); //                   |----> sub is a subject,which is a standard claim of JWT token,which is used to identify the subject of the JWT token...
+    //sign is a method from jsonwebtoken module,which will generate a token...
 
-  //process
-  //response
-  res.json({ accessToken: token }); //sending the token as a response...
+    //process
+    //response
+    res.json({ accessToken: token }); //sending the token as a response...
+  } catch (err) {
+    return next(createHttpError(500, "Server error @JWT token generation"));
+  }
 };
 
 export { createUser };
