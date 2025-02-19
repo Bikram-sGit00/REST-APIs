63f00f7321c35c038bd9de3c20631c5507d8864a
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-20 01:04:06 +0530

    add temo file delete feature

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,9 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
+import bookModel from "./bookModel";
+import fs from "node:fs";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   console.log("files", req.files);
+
+  const { title, genre } = req.body;
 
   const files = req.files as { [fieldname: string]: Express.Multer.File[] }; // Type assertion
   const coverImageMimeType = files.coverImage[0].mimetype.split("/")[1]; //splitting the mimetype and getting the second part of the array or we can do ".at(1)"...
@@ -29,7 +33,19 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     folder: "book-pdfs",
     format: "pdf",
   });
-  res.json({});
+  const newBook = await bookModel.create({
+    title,
+    genre,
+    author: "67b62ff795050caf9a923a14",
+    coverImage: uploadFile.secure_url,
+    file: uploadBookFile.secure_url,
+  });
+
+  // Delete the files after uploading to cloudinary
+  await fs.promises.unlink(filePath);
+  await fs.promises.unlink(bookFilePath);
+
+  res.status(201).json({ id: newBook._id });
 };
 
 export { createBook };
