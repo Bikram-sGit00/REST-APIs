d6b65b9d588a4638822caab10962798694c860a6
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-19 16:06:20 +0530

    change env file

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,6 +7,9 @@ const _config = {
   databaseUrl: process.env.MONGO_CONNECTION_STRING,
   env: process.env.NODE_ENV, // NODE_ENV is an environment variable that is used to know the environment in which the application is running...
   jwtSecret: process.env.JWT_SECRET,
+  cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
 };
 
 export const config = Object.freeze(_config); // freeze is an javascript object method,'object.freeze' is used to make the object read-only...
