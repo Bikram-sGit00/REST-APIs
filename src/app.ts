bbfd21d4be24db9977267ee22fd8fb1b9ea7a0ff
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-21 15:40:44 +0530

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 import bookRouter from "./book/bookRouter";
+import { config } from "./config/config";
 
 const app = express();
+app.use(
+  cors({
+    origin: config.frontendDomain,
+  })
+);
 app.use(express.json()); //middleware to parse Json data,by defalut it is disabled in express...
 
 app.get("/", (req, res, next) => {
